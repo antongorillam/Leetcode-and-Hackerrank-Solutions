050f72bb1c03e88841af2d87c24210146584dcd7
Anton <whafu@kth.se>
2022-10-10 22:47:34 +0200

    Finished `Minimum Depth of Binary Tree`

--- README.md
+++ README.md
@@ -11,9 +11,25 @@ Idea: It is easy to bruteforce $\mathcal{O}(n^2)$, but using hashtables can redu
 1. Loop over nums and save the results in a hashtable with value as key (I choosed to save the residual from the target, but does not matter)
 2. Loop over nums once again, but this time we use to hashtable to check if the corresponding answer exist, which is $\mathcal{O}(1)$.
 
-
 ### 013 Roman to Integer
 
+[Problem link](https://leetcode.com/problems/roman-to-integer/)
+
+Idea: For every roman symbol, if the symbol left of it is smaller than the one, add the roman symbol to the total sum, else subtract is:
+
+Furthermore will use hashtable linking each roman symbol to its value to make the code shorter.
+
 ### 101 Symmetric Tree
+
+### 111 Minimum Depth of Binary Tree
+
+[Problem link](https://leetcode.com/problems/minimum-depth-of-binary-tree/)
+
+Idea: Since we want to find the shortest height, *bfs* (breath-first-search) makes most sense, since it will check each "level" at a time.
+
+1. Create a queue and proceed with a normal bfs, with only difference is that we track which heigh each node is on.
+2. As soon as booth we find a leaf node (both children are `None`) then we know we have found the lowest height.
+
+Time complexity: $\mathcal{O}(|V|)$
 
 ## Hackerrank solutions
---
+++ leetcode_solutions/111_Minimum_Depth_of_Binary_Tree.py
@@ -0,0 +1,37 @@
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        
+        # First handle the edge case
+        if root==None:
+            return 0
+        
+        queue = []
+        # The 2nd element of the tuple note what height we are on
+        queue.append((root, 1)) 
+        
+        while len(queue) > 0:
+            (node, current_height) = queue.pop(0)
+            
+            # If both nodes are none, then we have found the lowest height
+            if (not node.left) and (not node.right):
+                return current_height
+                
+            if node.left:
+                l_node = node.left
+                queue.append((l_node, current_height+1))
+            
+            if node.right:
+                r_node = node.right
+                queue.append((r_node, current_height+1))
+
+"""
+Problem link : https://leetcode.com/problems/minimum-depth-of-binary-tree/
+
+Idea: Since we want to find the shortest height, bfs (breath-first-search) makes most sense, since it will check each "level" at a time.
+
+1. Create a queue and proceed with a normal bfs, with only difference is that we track which heigh each node is on.
+2. As soon as booth we find a leaf node (both children are None) then we know we have found the lowest height.
+
+Time complexity: O(|V|)
+
+"""
